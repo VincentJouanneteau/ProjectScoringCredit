de7626c22c94198445aa2da1dbe3c868618341c4
VincentJouanneteau <vincjou@yahoo.fr>
2024-05-23 14:26:16 +0200

    modifications tests unitaires

--- test/test_clients.py
+++ test/test_clients.py
@@ -4,13 +4,13 @@ import requests
         
 class TestRefusAcceptation(unittest.TestCase):
     def test_should_be_accepted(self):
-        url_api = "http://13.38.119.19:8080/score/100001"
+        url_api = "http://13.39.87.160:8080/score/100001"
         response = requests.get(url_api)
         decision = response.json()['prediction_text']
         self.assertEqual(decision,"Accepté")
     
     def test_should_be_refused(self):
-        url_api = "http://13.38.119.19:8080/score/100561"
+        url_api = "http://13.39.87.160:8080/score/100561"
         response = requests.get(url_api)
         decision = response.json()['prediction_text']
         self.assertEqual(decision,"Refusé")
