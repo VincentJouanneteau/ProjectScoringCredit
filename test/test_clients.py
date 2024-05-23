433cd047520f1426c60627a5e26693b2410ba0aa
VincentJouanneteau <vincjou@yahoo.fr>
2024-05-23 16:12:42 +0200

    changement test unitaire API local

--- test/test_clients.py
+++ test/test_clients.py
@@ -5,13 +5,13 @@ import requests
 class TestRefusAcceptation(unittest.TestCase):
     # Test du client 100001 qui doit être accepté
     def test_should_be_accepted(self):
-        url_api = "http://13.39.87.160:8080/score/100001"
+        url_local_api = "http://127.0.0.1:8080/score/100001"
         response = requests.get(url_api)
         decision = response.json()['prediction_text']
         self.assertEqual(decision,"Accepté")
     # Test du client 100561 qui doit être refusé
     def test_should_be_refused(self):
-        url_api = "http://13.39.87.160:8080/score/100561"
+        url_local_api = "http://127.0.0.1:8080/score/100561"
         response = requests.get(url_api)
         decision = response.json()['prediction_text']
         self.assertEqual(decision,"Refusé")
